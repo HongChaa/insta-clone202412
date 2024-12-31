538e06683df85dab0f12e1e257bbe6c07ae87aea
soongu <naong526@naver.com>
2024-12-31 15:39:56 +0900

    #09 [FE/feat] make indicators in Carousel

--- src/main/resources/static/js/ui/CarouselManager.js
+++ src/main/resources/static/js/ui/CarouselManager.js
@@ -8,6 +8,9 @@ class CarouselManager {
 
     // 이미지 트랙(실제 이미지가 배치될 공간)
     this.track = this.container.querySelector('.carousel-track');
+
+    // 인디케이터 영역
+    this.indicatorContainer = this.container.querySelector('.carousel-indicators');
     
     // 실제 이미지 파일 배열
     this.slides = [];
@@ -25,7 +28,8 @@ class CarouselManager {
     // 이미지 트랙 리셋
     this.track.innerHTML = '';
 
-    this.slides.forEach(file => { 
+    // 슬라이드 이미지 생성
+    this.slides.forEach((file, index) => { 
       // 이미지 생성
       const $img = document.createElement('img');
       // raw file을 image url로 변환
@@ -37,7 +41,19 @@ class CarouselManager {
       $slideDiv.append($img);
 
       this.track.append($slideDiv);
+
+      // 인디케이터 생성
+      if (this.slides.length > 1) this.makeIndicator(index);
     });
+  }
+
+  // 인디케이터 생성하기
+  makeIndicator(index) {
+    const $indicator = document.createElement('span');
+    $indicator.classList.add('indicator');
+    if (index === 0) $indicator.classList.add('active');
+
+    this.indicatorContainer.append($indicator);
   }
 
 }
