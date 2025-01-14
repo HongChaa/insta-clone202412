b93d5d107d0e5d6a4f4e7a22940bedd2740906e3
soongu <naong526@naver.com>
2025-01-14 11:57:15 +0900

    #79 [FE/BE/feat] render feed items includes real user info

--- src/main/resources/static/js/components/feed.js
+++ src/main/resources/static/js/components/feed.js
@@ -76,7 +76,7 @@ function truncateContent(writer, content, maxLength = 20) {
 
 
 // 한개의 피드를 렌더링하는 함수
-function createFeedItem({ writer, content, images, createdAt }) {
+function createFeedItem({ username, profileImageUrl, content, images, createdAt }) {
 
   // const makeImageTags = (images) => { 
   //   let imgTag = '';
@@ -91,12 +91,12 @@ function createFeedItem({ writer, content, images, createdAt }) {
       <div class="post-header">
         <div class="post-user-info">
           <div class="post-profile-image">
-            <img src="/images/default-profile.svg" alt="프로필 이미지">
+            <img src="${profileImageUrl || '/images/default-profile.svg'}" alt="프로필 이미지">
           </div>
           <div class="post-user-details">
             <a href="#" class="post-username">
                 <!--      작성자 이름 배치      -->
-                ${writer}
+                ${username}
             </a>
           </div>
         </div>
@@ -168,7 +168,7 @@ function createFeedItem({ writer, content, images, createdAt }) {
       <div class="post-content">
         <div class="post-text">
             <!--     피드 내용     -->
-            ${truncateContent(writer, content)}
+            ${truncateContent(username, content)}
         </div>
         <div class="post-time">
             <!--      피드 생성 시간      -->
@@ -191,6 +191,7 @@ function createFeedItem({ writer, content, images, createdAt }) {
 async function renderFeed() {
   // 피드 데이터를 서버로부터 불러오기
   const feedList = await fetchFeeds();
+  console.log(feedList);
 
   // feed html 생성
   $feedContainer.innerHTML = feedList.map((feed) => createFeedItem(feed)).join('');
